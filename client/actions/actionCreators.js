50ce71b0856f83d2c13cd9f2d422bb63eeeeebe8
Михаил <m.smirnov@cuberto.ru>
2017-11-27 15:39:01 +0300

    feature/ add actions

---
+++ client/actions/actionCreators.js
@@ -0,0 +1,25 @@
+// increment likes
+export function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index
+    }
+}
+// add comment
+export function addComment(postId, author, comment) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment
+    }
+}
+
+// remove comment
+export function addComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT',
+        postId,
+        i
+    }
+}
