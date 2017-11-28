ca793480021fd80dd0acaa124e3af0dacc82aba3
Smirnov Mikhail <urine89@mail.ru>
2017-11-29 00:51:39 +0300

    Add Photo to Single, add Comments to Single

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Photo from './Photo';
+// improt comments
+import Comments from './Comments';
 
 const Single = React.createClass({
     render() {
+        // index of the post
+        const {postId} = this.props.params;
+
+        const i = this.props.posts.findIndex(post => post.code === postId);
+        // actual post
+        const post = this.props.posts[i];
+
+        const comments = this.props.comments[postId] || [];
+
         return (
             <div className="single-photo">
-                i'm single
+                <Photo index={i} post={post} {...this.props} />
+                <Comments comments={comments}/>
             </div>
         )
     }
